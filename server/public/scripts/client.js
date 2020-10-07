34db5bbea8019416ec553ad9aee13fb1d1e13fb9
Sam Mahler <mahler.sam@gmail.com>
2020-10-07 16:01:30 -0500

    append functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,21 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQUERY');
-    
+    getRandomQuote();
+}
+
+function getRandomQuote() {
+    console.log('get the quote');
+    $.ajax({
+        method: 'GET',
+        url: '/randomQuote'
+    }).then(function(response){
+        appendToDom(response);
+    });
+}
+
+function appendToDom(dataToAppend) {
+    $('#output').append(`
+        ${dataToAppend.quote} -${dataToAppend.author}
+    `);
 }
