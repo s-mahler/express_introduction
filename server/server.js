eb25a88cc5d353d9c0e9bf8ac1a94590c9309ab5
Sam Mahler <mahler.sam@gmail.com>
2020-10-07 13:26:56 -0500

    caught up coding

--- server/server.js
+++ server/server.js
@@ -3,20 +3,27 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const quotesData = require('./modules/quotes.js');
+
 app.use(express.static('server/public'));
 
-let quotesData = [
-    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
-];
+
 
 let index = 0;
 
 app.get('/quotes', (req, res) => {
     console.log('hello from get req');
-    res.sendStatus(418);
+    res.send(quotesData);
 });
+
+app.get('/randomQuote', (req, res) => {
+    let randomNum = getRndInteger(0, quotesData.length);
+    res.send(quotesData[randomNum]);
+});
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
 
 app.listen(port, () => {
     console.log('Up and running on port: ', port);
